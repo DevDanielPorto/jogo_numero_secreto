c4a4a2cf8506fc03915828bf4816fdfc007e6b47
Daniel Porto <danielporto2622@gmail.com>
2026-08-22 18:07:03 -0300

    A1.3 - Manipulando textos

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escoçha um número entre 1 e 10';
